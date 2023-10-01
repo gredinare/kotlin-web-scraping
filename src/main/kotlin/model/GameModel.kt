ba37f2e1d167f6d417501384a14e77ab41cbbf66
personal <gabrielgredinare@outlook.com>
2023-09-30 23:33:44 -0300

    get release method

--- src/main/kotlin/model/GameModel.kt
+++ src/main/kotlin/model/GameModel.kt
@@ -3,8 +3,8 @@ package model
 data class GameModel(
     val id: Int,
     val name: String,
-    val release: String = "",
-    val platform: List<String> = listOf(),
-    val score: String = "",
-    val genre: String = ""
+    val release: String = "-",
+    val platform: List<String> = listOf("-"),
+    val score: String = "-",
+    val genre: String = "-"
 )
--- src/main/kotlin/parse/mobyGames/MBInfoBlock.kt
+++ src/main/kotlin/parse/mobyGames/MBInfoBlock.kt
@@ -3,8 +3,13 @@ package parse.mobyGames
 import org.jsoup.nodes.Element
 
 class MBInfoBlock(val infoBlock: Element) {
+    private val infoRelease = infoBlock.select(".info-release").first()?.select(".metadata")
+    private val infoScore = infoBlock.select(".info-score").first()?.select(".metadata")
+    private val infoGenres = infoBlock.select(".info-genres").first()?.select(".metadata")
+
     fun getRelease(): String {
-        return ""
+        val release = infoRelease?.select("dd")?.first()?.select("a")?.first()?.text()
+        return release ?: "-"
     }
 
     fun getPlatform(): List<String> {
